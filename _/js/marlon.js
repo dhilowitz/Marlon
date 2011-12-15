45c0c671e0a6dc4045b31518e4059d53319bd229
David Hilowitz <dhilowitz@gmail.com>
2011-12-15 07:39:24 -0600

    Added a playhead. It still needs to be styled, though.
    Signed-off-by: David Hilowitz <dhilowitz@gmail.com>

--- _/js/marlon.js
+++ _/js/marlon.js
@@ -3,19 +3,16 @@
 // Set some camera attributes
 var CAMERA_DIST = 700; 
 var CAMERA_ROTATION_STEP_AMOUNT = 5; // This is in degrees. 
+
+// Set world dimensions
 var SEQUENCER_STEP_WIDTH = 32;
 var NUMBER_OF_VOICES = 8;
 var NUMBER_OF_SEQUENCE_STEPS = 16;
 var NUMBER_OF_GRID_NOTES = 16;
 var GRID_WIDTH = NUMBER_OF_SEQUENCE_STEPS * SEQUENCER_STEP_WIDTH;
 var GRID_DEPTH = NUMBER_OF_GRID_NOTES * SEQUENCER_STEP_WIDTH;
-var VOICE_COLORS = colorRainbowArray(16,0, false, 128, 127);
+var VOICE_COLORS = colorRainbowArray(NUMBER_OF_VOICES*2,0, false, 128, 127);
 var SCALE_BASE_NOTE = 57;
-
-
-// This is left over from Tito. Hmm. Do we want to have a fixed grid size?
-//var GRID_WIDTH = 736;
-//var GRID_HEIGHT = 449;
 
 function Note() {
 	var on = false;
@@ -88,7 +85,8 @@ function Marlon (marlonCanvasID)
 	this.WIDTH = window.innerWidth - 0;
 	this.HEIGHT = window.innerHeight - 0;
 	
-	this.rotationDegrees = -45;
+	this.rotationDegreesHorizontal = -45;
+	this.rotationDegreesVertical = 45;
 	this.playheadPosition = 0;
 	this.currentVoice = 0;
 	this.currentStep = 0;
@@ -101,10 +99,10 @@ function Marlon (marlonCanvasID)
 	this.setupVoices();
 	this.setupCubes();
 	this.setupCursor();
+	this.setupPlayhead();
 	this.dimAndUndimNotes();
 	
 }
-
 
 Marlon.prototype.drawGrid = function() {
 	//Grid
@@ -136,7 +134,7 @@ Marlon.prototype.dimAndUndimNotes = function() {
 		} else {
 			currentColor = 0x336666;
 			currentColor = VOICE_COLORS[i];
-			opacity = 0.7;
+			opacity = 0.2;
 			visible = true;
 			wireframe = false;
 		}
@@ -161,6 +159,25 @@ Marlon.prototype.setupCursor = function() {
 	this.calculateCursorPosition();
 }
 
+Marlon.prototype.setupPlayhead = function() {
+	var geometry = new THREE.CubeGeometry( SEQUENCER_STEP_WIDTH, 
+		NUMBER_OF_VOICES * SEQUENCER_STEP_WIDTH, //Height
+		NUMBER_OF_GRID_NOTES * SEQUENCER_STEP_WIDTH);
+    var material = new THREE.MeshBasicMaterial( { color: 0x336666, wireframe: true,opacity: 0.7 } );
+	this.playhead = new THREE.Mesh( geometry, material );
+
+	this.scene.add( this.playhead);
+	
+	this.calculatePlayheadPosition();
+}
+
+
+Marlon.prototype.calculatePlayheadPosition =  function() {
+	this.playhead.position.x = SEQUENCER_STEP_WIDTH*this.playheadPosition + (SEQUENCER_STEP_WIDTH/2);
+	this.playhead.position.y = ((NUMBER_OF_VOICES * SEQUENCER_STEP_WIDTH)/2);
+	this.playhead.position.z = (NUMBER_OF_GRID_NOTES * SEQUENCER_STEP_WIDTH)/2;
+}
+
 Marlon.prototype.calculateCursorPosition = function() {
 	this.cursorCube.position.x = (SEQUENCER_STEP_WIDTH/2) + this.currentStep * SEQUENCER_STEP_WIDTH;// - (256/2);
 	this.cursorCube.position.y = (SEQUENCER_STEP_WIDTH/2) + this.currentVoice * SEQUENCER_STEP_WIDTH;
@@ -174,10 +191,10 @@ Marlon.prototype.calculateCursorPosition = function() {
 		{	
 			var cursorIsOnTopOfCurrentNote = ((this.voices[i].sequence.notes[j].midiNote == this.currentNote) && (i == this.currentVoice) && (j == this.currentStep));
 			
-			if(cursorIsOnTopOfCurrentNote == true)
-				this.voices[i].sequence.notes[j].cube.material.opacity = 1;
-			else
-				this.voices[i].sequence.notes[j].cube.material.opacity = 0.7;
+			//if(cursorIsOnTopOfCurrentNote == true)
+			//	this.voices[i].sequence.notes[j].cube.material.opacity = 1;
+			//else
+			//	this.voices[i].sequence.notes[j].cube.material.opacity = 0.7;
 			
 			if(cursorIsOnTopOfCurrentNote)
 				cursorIsOnTopOfANote = true;
@@ -308,16 +325,28 @@ Marlon.prototype.setupCubes = function() {
 		for(var stepIndex=0; stepIndex < currentVoice.sequence.notes.length; stepIndex++)
 		{
 			currentNote = currentVoice.sequence.notes[stepIndex];
-			if(currentNote.midiNote == -1) continue;
+			if(currentNote.midiNote == -1) {
+				if(typeof(currentNote.cube) != 'undefined' && currentNote.cube.visible == true) {
+					currentNote.cube.visible = false;
+				}
+				
+				continue;
+			}
 			
 			//Cube
-			var geometry = new THREE.CubeGeometry(SEQUENCER_STEP_WIDTH, SEQUENCER_STEP_WIDTH, SEQUENCER_STEP_WIDTH);
-		    var material = new THREE.MeshBasicMaterial({
+			var material = new THREE.MeshBasicMaterial({
 			        color: currentVoice.color,
 			        opacity: 0.7,
 			        wireframe: false
 			    })
-			currentNote.cube = new THREE.Mesh( geometry, material );
+			
+			if(typeof(currentNote.cube) == 'undefined') {
+				var geometry = new THREE.CubeGeometry(SEQUENCER_STEP_WIDTH, SEQUENCER_STEP_WIDTH, SEQUENCER_STEP_WIDTH);
+		    	currentNote.cube = new THREE.Mesh( geometry, material );
+			} else{
+			
+				currentNote.cube.visible = true;
+			}
 			currentNote.cube.position.x = (SEQUENCER_STEP_WIDTH/2) + stepIndex * SEQUENCER_STEP_WIDTH;// - (256/2);
 			currentNote.cube.position.y = (SEQUENCER_STEP_WIDTH/2) + voiceIndex * SEQUENCER_STEP_WIDTH;
 			currentVoice.sequence.notes[stepIndex].cube = currentNote.cube;
@@ -331,29 +360,25 @@ Marlon.prototype.setupCubes = function() {
 	}
 }
 
-/*
-Marlon.prototype.animate =  function animate() {
-	requestAnimationFrame( animate );
-	
-    this.render();
-}
-*/
 Marlon.prototype.render =  function() {
 //	this.movePlayhead(); this.calculatePlayheadPosition();
-//	this.rotationDegrees += 1;
+//	this.rotationDegreesHorizontal += 1;
 //	this.calculateCameraPosition();
 	this.renderer.render( this.scene, this.camera );
 	this.stats.update();
 }
 
-Marlon.prototype.calculatePlayheadPosition =  function() {
-	this.playheadGrid.position.x = SEQUENCER_STEP_WIDTH*this.playheadPosition - (240);
-}
 
 Marlon.prototype.calculateCameraPosition =  function() {
-	var radians = (this.rotationDegrees / 180) * (Math.PI);
-	this.camera.position.z = CAMERA_DIST * Math.cos(radians) + GRID_DEPTH/2;
-	this.camera.position.x = CAMERA_DIST * Math.sin(radians) + GRID_WIDTH/2;
+	
+	var radiansHorizontal = (this.rotationDegreesHorizontal / 180) * Math.PI;
+	var radiansVertical = (this.rotationDegreesVertical / 180) * Math.PI;
+	
+	this.camera.position.x = CAMERA_DIST * Math.sin(radiansHorizontal) + GRID_WIDTH/2;
+	this.camera.position.z = CAMERA_DIST * Math.cos(radiansHorizontal) + GRID_DEPTH/2;
+	
+	//this.camera.position.y = CAMERA_DIST * Math.sin(radiansVertical) + GRID_WIDTH/2;
+	//this.camera.position.z = CAMERA_DIST * Math.cos(radiansVertical) + GRID_DEPTH/2;
 	
 	if(typeof(this.grid) != 'undefined')
 		this.camera.lookAt(this.grid.position);
@@ -372,14 +397,13 @@ Marlon.prototype.setupEventHandlers = function() {
 	$(document.body).mouseout(function(event) {thisObject.onMouseUp(event);});
 }
 
-
 Marlon.prototype.onMouseDown = function onMouseDown(event)
 {
 	
 	this.mouseXOnMouseDown = event.pageX;
 	this.mouseYOnMouseDown = event.pageY;
 	d("mouseXOnMouseDown: " + this.mouseXOnMouseDown);
-	this.rotationDegreesOnMouseDown = this.rotationDegrees;
+	this.rotationDegreesHorizontalOnMouseDown = this.rotationDegreesHorizontal;
 	d('onMouseDown');
 	
 	var thisObject = this;
@@ -387,13 +411,12 @@ Marlon.prototype.onMouseDown = function onMouseDown(event)
 	$(document.body).bind('mouseup',function(event) {thisObject.onMouseUp(event);});
 }
 
-
 Marlon.prototype.onMouseMove = function onMouseMove(event)
 {
 	this.mouseX = event.pageX;
 	var mouseRotationSensitivity = 0.2;
 	var mouseXDelta = (this.mouseX - this.mouseXOnMouseDown);
-	this.rotationDegrees = this.rotationDegreesOnMouseDown - (mouseXDelta * mouseRotationSensitivity);// * -0.12;
+	this.rotationDegreesHorizontal = this.rotationDegreesHorizontalOnMouseDown - (mouseXDelta * mouseRotationSensitivity);// * -0.12;
 	this.calculateCameraPosition();
 }
 
@@ -412,12 +435,12 @@ Marlon.prototype.onMouseClick = function onMouseClick(evt)
 Marlon.prototype.onKeyDown = function onKeyDown(event) {
 	switch(event.which) {
 		case 188: //',' key
-			this.rotationDegrees -= CAMERA_ROTATION_STEP_AMOUNT;
+			this.rotationDegreesHorizontal -= CAMERA_ROTATION_STEP_AMOUNT;
 			this.calculateCameraPosition();
 			event.preventDefault();
 			break;
 		case 190: //'.' key
-			this.rotationDegrees += CAMERA_ROTATION_STEP_AMOUNT;
+			this.rotationDegreesHorizontal += CAMERA_ROTATION_STEP_AMOUNT;
 			this.calculateCameraPosition();
 			event.preventDefault();
 			break;
@@ -444,8 +467,18 @@ Marlon.prototype.onKeyDown = function onKeyDown(event) {
 			event.preventDefault();
 			break;
 		case 48: // '0' arrow
-			this.rotationDegrees = -45;
+			this.rotationDegreesHorizontal = -45;
 			this.calculateCameraPosition();
+			event.preventDefault();
+			break;
+		case 18: // option key
+			if(this.voices[this.currentVoice].sequence.notes[this.currentStep].midiNote != this.currentNote)
+				this.voices[this.currentVoice].sequence.notes[this.currentStep].midiNote = this.currentNote;
+			else
+				this.voices[this.currentVoice].sequence.notes[this.currentStep].midiNote = -1;
+				
+			this.setupCubes();
+				
 			event.preventDefault();
 			break;
 		case 88: // 'x' arrow
@@ -466,12 +499,12 @@ Marlon.prototype.onKeyDown = function onKeyDown(event) {
 				event.preventDefault();
 				break;
 		case 189: // '-' key
-			this.camera.position.y-=10;
+			this.rotationDegreesVertical -= CAMERA_ROTATION_STEP_AMOUNT;
 			this.calculateCameraPosition();
 			event.preventDefault();
 			break;
 		case 187: // '+' key
-			this.camera.position.y+=10;
+			this.rotationDegreesVertical += CAMERA_ROTATION_STEP_AMOUNT;
 			this.calculateCameraPosition();
 			event.preventDefault();
 			break;
@@ -487,7 +520,7 @@ Marlon.prototype.onKeyDown = function onKeyDown(event) {
 
 	var msg = "Handler for .keypress() called " + this.xTriggered + " time(s). Keycode was " + event.which + "\n";
 	msg += "Camera:\n";
-	msg += "Angle: " + this.rotationDegrees + " ";	
+	msg += "Angle: " + this.rotationDegreesHorizontal + " ";	
 	msg += "x: " + this.camera.position.x + " / ";	
 	msg += "y: " + this.camera.position.y + " / ";	
 	msg += "z: " + this.camera.position.z + ".\n";
@@ -497,7 +530,6 @@ Marlon.prototype.onKeyDown = function onKeyDown(event) {
 	d(msg);		
 
 }
-
 
 Marlon.prototype.drawDebugAxes = function drawDebugAxes(){           
     //Axes array[x,y,z]
