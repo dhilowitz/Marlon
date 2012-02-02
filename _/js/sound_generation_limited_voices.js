eeea1423336d7686209703e9f2d9b4737a102ba8
David Hilowitz <dhilowitz@gmail.com>
2012-02-02 01:10:29 -0600

    Added a new, shorter synth patch.
    Signed-off-by: David Hilowitz <dhilowitz@gmail.com>

--- _/js/sound_generation_limited_voices.js
+++ _/js/sound_generation_limited_voices.js
@@ -9,7 +9,14 @@ SoundGeneration.prototype.init = function init()
 	for(var voiceIndex = 0; voiceIndex < this.numVoices; voiceIndex++)
 	{
 		this.voices[voiceIndex] = new Audio();
-		this.voices[voiceIndex].autoplay = true;
+		//this.voices[voiceIndex].autoplay = true;
+	}
+}
+
+SoundGeneration.prototype.stopAllNotes = function stopAllNotes() {
+	for(var voiceIndex = 0; voiceIndex < this.numVoices; voiceIndex++)
+	{
+		this.voices[voiceIndex].pause();
 	}
 }
 
@@ -32,10 +39,12 @@ SoundGeneration.prototype.playNote = function playNote(noteNumber)
 
 SoundGeneration.prototype.getNoteURL = function getNoteURL(noteNumber)
 {
-	if(noteNumber >= 48 && noteNumber <= 112)
+	noteNumber = noteNumber - 12 - 12;
+	//if(noteNumber >= 48 && noteNumber <= 112)
+	if(true)
 	{
 		//return "autoharp/mp3/" + zeroPad(noteNumber, 3) + ".mp3"; 
-		return "moog_12_c_hendre/mp3/" + encodeURIComponent(ConvertMIDINoteNumberToNoteName(noteNumber)) + ".mp3"; 
+		return "moog_12_c_hendre_medium/wav/" + encodeURIComponent(ConvertMIDINoteNumberToNoteName(noteNumber)) + ".wav"; 
 		
 	}
 	return null;
